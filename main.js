39ad8f48877359d10475649c0b66ebedb802b64a
Nikola Jandrić <nikola.jandric@outlook.com>
2017-10-30 23:05:17 +0100

    added selectors, defined main variables.

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+//#region - selectors
+const album = Number(prompt("Koliko album ima sličica?"));
+const input = document.getElementById("inputSticker");
+const table = document.getElementById("table");
+const calcButton = document.getElementById("calc");
+const printButton = document.getElementById("print");
+const displayResult = document.getElementById("result");
+//#endregion
+
+let stickers = 0;
