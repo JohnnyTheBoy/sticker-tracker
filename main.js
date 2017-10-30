6fe0b41699cfc5655daa201bb2f7084ccb66a801
Nikola Jandrić <nikola.jandric@outlook.com>
2017-10-30 23:06:38 +0100

    table func - format table based on album

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 
 
 //#region - selectors
-const album = Number(prompt("Koliko album ima sličica?"));
 const input = document.getElementById("inputSticker");
 const table = document.getElementById("table");
 const calcButton = document.getElementById("calc");
@@ -9,4 +8,17 @@ const printButton = document.getElementById("print");
 const displayResult = document.getElementById("result");
 //#endregion
 
+const album = Number(prompt("Koliko album ima sličica?"));
 let stickers = 0;
+
+//#region - Format table based on number of stickers in album
+let table = (album) => {
+    for (let i = 1; i <= album; i++) {
+        let sticker = document.createElement("td");
+        sticker.textContent = i;
+        row.appendChild(sticker);
+    }
+}
+//#endregion
+
+table(album);
